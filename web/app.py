066a9d75e2a6289840fc2baf0e83243bc2367545
Yut <121371082+NDarayut@users.noreply.github.com>
2025-01-28 16:11:56 +0700

    Update app.py

--- web/app.py
+++ web/app.py
@@ -97,8 +97,3 @@ def predict():
                 os.remove(image_path)
             return render_template('index.html', prediction=f"Error: {str(e)}", image=None)
 
-
-if __name__ == '__main__':
-    # Get the port from environment variables or default to 5000
-    port = int(os.environ.get("PORT", 10000))
-    app.run(debug=True, host="0.0.0.0", port=port)
