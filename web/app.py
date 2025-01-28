f01c0289a660a7365b95778fcaac3e3f4b2a0458
Yut <121371082+NDarayut@users.noreply.github.com>
2025-01-28 15:20:29 +0700

    Update app.py

--- web/app.py
+++ web/app.py
@@ -99,5 +99,6 @@ def predict():
 
 
 if __name__ == '__main__':
-
-    app.run(debug=True)
+    # Get the port from environment variables or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
